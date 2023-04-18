1441e2c750c36d177f2edd5064baf1beb126f7b5
uBing0101 <jeonyb0101@naver.com>
2023-04-18 18:01:21 +0900

    2023/04/18 2차 배포

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
 const Nav = () => {
+
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if(window.scrollY > 50) {
+        setShow(true);
+      }else{
+        setShow(false);
+      }
+    })
+
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    }
+
+  }, [])
   return (
-    <NavWrapper>
+    <NavWrapper show={show}>
       <Logo>
         <img 
           alt='Disney Plus Logo'
@@ -23,13 +40,14 @@ const NavWrapper = styled.nav`
   left: 0;
   right: 0;
   height: 70px;
-  background-color: #090b13;
+  background-color: ${props => props.show ? "#090b13" : "transparent"};
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 0 36px;
   letter-spacing: 16px;
   z-index: 3;
+  transition: all .3s;
 `
 
 const Logo = styled.a`
